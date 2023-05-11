f769de1b6436a7e4340380e7c6c774c6d99da15e
Alvaro Gracia <alvaro.gracia.reg@gmail.com>
2023-05-11 23:45:00 +0200

    clean node

---
+++ generateList.js
@@ -0,0 +1,29 @@
+/*Project for leaning and testing how api works
+
+i want to ask for data and retrieve another for testing both*/
+
+/**
+ * The function creates an array of objects representing students with random names and grades.
+ * @param length - The length parameter is the number of elements that the array will have. It is used
+ * to determine the size of the array and how many times the for loop will run to create new objects to
+ * add to the array.
+ * @returns an array of objects representing students, with each object containing an id, a randomly
+ * generated name from an array of names, and a randomly generated grade between 0 and 10. The length
+ * of the array is determined by the input parameter "length".
+ */
+function createArray(length){
+
+	var cantidadDeAlumnos 	= length;
+	var alumnosArray 		= new Array(cantidadDeAlumnos)
+	
+	for (var n = 0; n < cantidadDeAlumnos; n++) {
+		var newAlumno = {
+			id: n + 1,
+			nombre: nombres[Math.floor(Math.random() * nombres.length)],
+			nota: Math.floor(Math.random() * 11)
+		}
+	
+		alumnosArray[n] = newAlumno
+	}
+	return alumnosArray
+}
